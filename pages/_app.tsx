f2e6d98e5932bff12d98fdd5400369c1f5fd66d4
Ridwan Abiola <ricklick262@gmail.com>
2023-07-03 15:30:22 +0100

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, metamaskWallet, paperWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
+import {PaperWallet} from "@thirdweb-dev/wallets"
+import { FantomTestnet } from "@thirdweb-dev/chains"
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+
+const emailWallet = paperWallet({
+  clientId: 'beeeed4c-946a-4a53-b4ba-b952cec3c21e',
+})
+
+const wallet = new PaperWallet({
+  chain: FantomTestnet,
+  clientId: ''
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={ FantomTestnet } supportedWallets={[trustWallet(), metamaskWallet(), walletConnect(), emailWallet]}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
